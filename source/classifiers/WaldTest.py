c7c00bd927880c3429f6236f84e01bc80b485491
Marina <ivanovicmimi@gmail.com>
2020-01-07 21:08:04 +0100

    Wald test

---
+++ source/classifiers/WaldTest.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+class WaldTest():
+
+    def __init__(self, f1=None, f2=None, priors=np.array([0.5, 0.5])):
+        '''
+            Constructor.
+            Args:
+                f1 (function): PDF for the first class
+                f2 (function): PDF for the second class
+                priors (numpy array of doubles): Priors for every class.
+        '''
+        self.f1 = f1
+        self.f2 = f2
+        self.priors = priors
+        self.compare_value = np.log(self.priors[0] / self.priors[1])
+
+    def predict_class(self, X, eps1, eps2):
+        '''
+            Predicts class for the given data.
+            Args:
+                X (numpy array): Data
+                eps1 (float): error for the prediction of the first class
+                eps2 (float): error for the prediction of the second class
+        '''
+        if self.f1 is None or self.f2 is None or self.compare_value is None:
+            return None
+        if len(X.shape) == 1:
+            X = np.array([X])
+        
+        # Caulculate values a and b
+        self.a = -np.log((1- eps1) / eps2)
+        self.b = -np.log(eps1 / (1- eps2))
+        
+        # Calculate s_m
+        self.m = 0
+        self.s_m = 0
+        for i in range(X.shape[0]):
+            h = -np.log(self.f1(X[i, :]) / self.f2(X[i, :]))
+
+            self.m += 1
+            self.s_m += h
+
+            # Is the test over?
+            if self.s_m <= self.a:
+                return 0
+            if self.s_m >= self.b:
+                return 1
+
+        # What if there was no decision?
+        return None
+
+    def __getitem__(self, key):
+        if key == 'm':
+            return self.m
+        if key == 's_m':
+            return self.s_m
+        if key == 'a':
+            return self.a
+        if key == 'b':
+            return self.b
+        return None
---
+++ source/test/test_wald.py
@@ -0,0 +1,80 @@
+import seaborn as sns
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.utils import shuffle
+from scipy.stats import multivariate_normal
+from ..utils import datagen
+from ..utils import dataplot
+from ..classifiers.WaldTest import WaldTest
+
+sns.set()
+plt.rcdefaults()
+
+def get_data(plt):
+    N = 500
+
+    # Class 1
+    M1 = [1, 1]
+    S1 = [[4, 1.1], [1.1, 2]]
+    M2 = [6, 4]
+    S2 = [[3, -0.8], [-0.8, 1.5]]
+    P1 = 0.6
+    x1 = datagen.generate_bimodal_gauss(M1, S1, M2, S2, P1, N)
+
+    # Class 2
+    M1 = [7, -9]
+    S1 = [[2, 1.1], [1.1, 4]]
+    M2 = [6, -5]
+    S2 = [[3, 0.8], [0.8, 0.5]]
+    P1 = 0.55
+    x2 = datagen.generate_bimodal_gauss(M1, S1, M2, S2, P1, N)
+
+    plt = dataplot.data_plot(plt, np.array([x1, x2]))
+    return plt, x1, x2
+
+def f1(x, y):
+    M1 = [1, 1]
+    S1 = [[4, 1.1], [1.1, 2]]
+    M2 = [6, 4]
+    S2 = [[3, -0.8], [-0.8, 1.5]]
+    P1 = 0.6
+
+    return P1 * multivariate_normal(mean=M1, cov=S1).pdf(np.array([x, y])) + (
+           (1-P1) * multivariate_normal(mean=M2, cov=S2).pdf(np.array([x, y])))
+
+def f2(x, y):
+    M1 = [7, -9]
+    S1 = [[2, 1.1], [1.1, 4]]
+    M2 = [6, -5]
+    S2 = [[3, 0.8], [0.8, 0.5]]
+    P1 = 0.55
+
+    return P1 * multivariate_normal(mean=M1, cov=S1).pdf(np.array([x, y])) + (
+           (1-P1) * multivariate_normal(mean=M2, cov=S2).pdf(np.array([x, y])))
+
+def f(x, y):
+    return f1(x, y) + f2(x, y)
+
+def f1_vec(x):
+    return f1(x[0], x[1])
+
+def f2_vec(x):
+    return f2(x[0], x[1])
+
+if __name__ == '__main__':
+    # Generate and plot data
+    figure_data, x1, x2 = get_data(plt)
+
+    # Predict outputs with Bayes classsifier
+    wald = WaldTest(f1_vec, f2_vec)
+
+    # Predict for the first class
+    y1 = wald.predict_class(x1, 1e-20, 1e-40)
+    print('Predicted class: {}. Real class: 0. Number of steps: {}.'.format(
+          y1, wald['m']))
+
+    # Predict for the second class
+    y2 = wald.predict_class(x2, 1e-20, 1e-40)
+    print('Predicted class: {}. Real class: 1. Number of steps: {}.'.format(
+          y2, wald['m']))
