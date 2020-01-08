5407761ef7f917c127bc66349b021826618484d2
Marina <ivanovicmimi@gmail.com>
2020-01-08 04:24:22 +0100

    test for square error clustering

---
+++ source/test/test_square_clustering.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.utils import shuffle
+from ..utils import datagen
+from ..clustering.kmeans import kmeans
+
+def get_data():
+    N = 500
+
+    # Class 0
+    c_x1 = 5.5
+    c_x2 = 7.5
+    angle = 0.25
+    dist = 1.5
+    x1 = datagen.generate_uniform_doughnut_part(c_x1, c_x2, angle, dist, N)
+
+    # Class 1
+    c_x1 = 6
+    c_x2 = 6.5
+    x2 = datagen.generate_uniform_circle(c_x1, c_x2, N)
+
+    data = np.concatenate((x1, x2), axis=0)
+    return shuffle(data)
+
+if __name__ == '__main__':
+    # Generate 2 non-separable classes
+    data = get_data()
+
+    #assignments, centers, iters = kmeans.kmeans(data, 2)
