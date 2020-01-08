133b3a336346dec559ab2155e355a8065072c996
Marina <ivanovicmimi@gmail.com>
2020-01-08 04:13:32 +0100

    kmeans plot to dataplot

--- source/clustering/kmeans/kmeans.py
+++ source/clustering/kmeans/kmeans.py
@@ -1,34 +1,14 @@
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 import random
-
-sns.set()
-plt.rcdefaults()
-cmap = ListedColormap(['red', 'green', 'blue', 'magenta'])
-
-def clusters_plot(X, Y, assignments=None, centers=None, title=None):
-    if assignments is None:
-        assignments = [0] * len(X)
-    fig = plt.figure(figsize=(14,8))    
-    
-    # Plot colored data and clusters center
-    plt.scatter(X, Y, c=assignments, cmap=cmap)
-    if centers is not None:
-        plt.scatter(centers[:, 0], centers[:, 1],
-                    marker='+', s=400, color='black', label='cluster center')
-    plt.title(title)
-    plt.legend()
-    plt.show()
+from ...utils import dataplot
 
 def kmeans(data, k):
     # Initial values
     n_examples = data.shape[0]
     centers = data[random.sample(range(n_examples), k)]
     assignments = [0] * n_examples
-    clusters_plot(data[:, 0], data[:, 1], assignments, centers,
-                 title='Data and initial centers')
+    dataplot.clusters_plot(data[:, 0], data[:, 1], assignments, centers,
+                           title='Data and initial centers')
     
     # Stop criterions
     delta = 100
@@ -61,6 +41,6 @@ def kmeans(data, k):
 
         iters += 1 
 
-    clusters_plot(data[:, 0], data[:, 1], assignments, centers,
-                  title='Final clusters and their centers')
+    dataplot.clusters_plot(data[:, 0], data[:, 1], assignments, centers,
+                           title='Final clusters and their centers')
     return assignments, centers, iters
--- source/utils/dataplot.py
+++ source/utils/dataplot.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from matplotlib import ticker, cm
+from matplotlib.colors import ListedColormap
 
 sns.set()
 plt.rcdefaults()
-data_colors = ['bo', 'ro', 'go', 'yo']
 
 def data_plot(plt, x):
+    data_colors = ['bo', 'ro', 'go', 'yo']
     for i in range(x.shape[0]):
         plt.plot(x[i, :, 0], x[i, :, 1], data_colors[i],
                  label='Class {}'.format(i)
@@ -35,4 +36,19 @@ def plot_f(plt, f, x1=-10, x2=10, x3=-10, x4=10, cmap=None, title=None):
     f = plt.contourf(X, Y, Z, cmap=cmap)
     plt.colorbar(f)
     plt.title(title)
+    plt.show()
+
+def clusters_plot(X, Y, assignments=None, centers=None, title=None):
+    cmap = ListedColormap(['red', 'green', 'blue', 'magenta'])
+    if assignments is None:
+        assignments = [0] * len(X)
+    fig = plt.figure(figsize=(14,8))    
+    
+    # Plot colored data and clusters center
+    plt.scatter(X, Y, c=assignments, cmap=cmap)
+    if centers is not None:
+        plt.scatter(centers[:, 0], centers[:, 1],
+                    marker='+', s=400, color='black', label='cluster center')
+    plt.title(title)
+    plt.legend()
     plt.show()
